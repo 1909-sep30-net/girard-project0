e5eed765ab0c105ad330a4fb4874c3fbf0d1740f
Girard Sanford <girard.sanford@gmail.com>
2019-10-04 15:12:15 -0500

    created movie, store, customer, and order classes

--- Project0/Business_Logic/Customer.cs
+++ Project0/Business_Logic/Customer.cs
@@ -1,12 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace BusinessLibrary
 {
     public class Customer
     {
         public int CustomerId { get; set; }
-        public string FirstName { get; set; }
-        public string lastname { get; set; }
+        public string FirstName
+        {
+            get 
+            {
+                return FirstName;
+            } set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+                this.FirstName = value;
+            } 
+        }
+        public string LastName 
+        {
+            get
+            {
+                return LastName;
+            }
+            set
+            {
+                if (value == null)
+                {
+                    throw new ArgumentNullException(nameof(value));
+                }
+                this.LastName = value;
+            }
+        }
+
+        public List<Order> OrderHistory = new List<Order>();
+
+        public Customer(int Id, string FName, string LName)
+        {
+            this.CustomerId = Id;
+            this.FirstName = FName;
+            this.LastName = LName;
+        }
+
+        public void LogOrder(Order order)
+        {
+            OrderHistory.Add(order);
+        }
+
+        public void DisplayOrderHistory(Customer c)
+        {
+            foreach (Order o in c.OrderHistory)
+            {
+                Console.WriteLine(o);
+            }
+        }
 
     }
 }
---
+++ Project0/Business_Logic/Order.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLibrary
+{
+    public class Order
+    {
+        public string StoreLocation { get; set; }
+        
+        public Customer Customer { get; set; }
+        public DateTime OrderDate { get; }
+        public List<Movie> MovieList = new List<Movie>();
+
+        public Order (Customer c, string sLocation, DateTime date)
+        {
+            this.StoreLocation = sLocation;
+            this.Customer = c;
+            this.OrderDate = date;
+        }
+
+    }
+}
