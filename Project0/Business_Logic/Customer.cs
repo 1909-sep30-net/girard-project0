426187c16c33b116fa85b6a8c18724f1d792d01a
Girard Sanford <girard.sanford@gmail.com>
2019-10-04 10:13:16 -0500

    Starting project0

---
+++ Project0/Business_Logic/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusinessLibrary
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string lastname { get; set; }
+
+    }
+}
